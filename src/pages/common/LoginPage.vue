f9beeccc1898877e644cb407c222b07e0d124097
jdev2807 <jdev7k48kc@gmail.com>
2022-09-25 00:37:06 +0700

    fixx

--- src/pages/common/LoginPage.vue
+++ src/pages/common/LoginPage.vue
@@ -38,16 +38,7 @@
                     placeholder="*********"
                 />
               </div>
-              <div class="flex justify-between items-center mb-6">
-                <div class="form-group form-check">
-                  <input
-                      type="checkbox"
-                      class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                      id="RememberMe"
-                      checked
-                  />
-                </div>
-              </div>
+
             <!--notification-->
               <p class="text-red-500 text-xs italic">không có pass đâu vào đi .</p>
               <!-- Submit -->
